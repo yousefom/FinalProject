b315e3187c7320dcc7d688c9b82fffa7701e8c04
yousefom <94544934+yousefom@users.noreply.github.com>
2021-12-13 17:11:20 +0200

    Add files via upload

---
+++ ADC_Config.h
@@ -0,0 +1,22 @@
+/*
+ * ADC_Config.h
+ *
+ *  Created on: Oct 2, 2021
+ *      Author: gerges
+ */
+
+#ifndef ADC_CONFIG_H_
+#define ADC_CONFIG_H_
+
+/*ADC Reference Volt Options:
+ * 1- ADC_AVCC
+ * 2- ADC_INTERNAL_2.56V
+ */
+#define ADC_Reference_Volt  ADC_AVCC
+
+/*ADC Left or  Right Adj Options:
+ * 1- ADC_LEFT_ADJUST
+ * 2- ADC_RIGHT_ADJUST
+ */
+#define ADC_LEFT_RIGHT_ADJUST  ADC_RIGHT_ADJUST
+#endif /* ADC_CONFIG_H_ */
